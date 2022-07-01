6b70893f58d395fa6691cbe8e0a7c5e1e15aff73
Alex <petenkov2822@gmail.com>
2022-07-01 10:57:03 +0300

    migration create

---
+++ db/migrations/20220701075639-create-answer.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question_id: {
+        type: Sequelize.INTEGER
+      },
+      answer: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Answers');
+  }
+};
