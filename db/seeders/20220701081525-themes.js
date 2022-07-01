7a35a69c99b78bbcfe8aa9b822b6b426a1135cea
Alex <petenkov2822@gmail.com>
2022-07-01 11:23:53 +0300

    Seeds from themes

---
+++ db/seeders/20220701081525-themes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('Themes', [{
+      desc: 'Java script для умных',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      desc: 'Сериалы',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      desc: 'Блатная',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      desc: 'Elbrus',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+ 
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Themes', null, {});    
+  }
+};
