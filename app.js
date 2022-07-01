875c8bdd6bda84b60d8e9e9b8f9ac6f086fcd80f
Damir <demchikbig777@mail.ru>
2022-07-01 11:10:55 +0300

    Morgan+MiddleWares

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ require('@babel/register');
 
 
 const express = require ('express');
+const logger = require('morgan');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT ?? 3000; 
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req,res) => {
   res.send('мама я попал на тиви')
