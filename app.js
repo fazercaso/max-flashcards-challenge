21ac170c797c1d53a4726f05350af34661f639a6
Damir <demchikbig777@mail.ru>
2022-07-01 10:56:18 +0300

    Mama ya popal na TV

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require ('express');
 const app = express();
 const PORT = process.env.PORT ?? 3000; 
 
-
+app.get('/', (req,res) => {
+  res.send('мама я попал на тиви')
+})
 
 
 app.listen(PORT, () => {
